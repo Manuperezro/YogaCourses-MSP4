69f8fb60cf8be6ec4c5000299cb233a47833929b
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-05 12:14:18 +0000

    created the subscription models to add subscriptions type to users using Free as default

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Pricing, Subscription
 # Register your models here.
 
 admin.site.register(Student)
+admin.site.register(Pricing)
+admin.site.register(Subscription)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.utils.text import slugify
+from django.db.models.signals import post_save, pre_save
 
 # Students Model
 class Student(models.Model):
@@ -11,8 +12,37 @@ class Student(models.Model):
     def __str_(self):
         return self.user.username
 
+class Pricing(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, blank=True, unique=True)
+    stripe_price_id = models.CharField(max_length=100, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    currency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Subscription(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    pricing = models.ForeignKey(Pricing, on_delete=models.CASCADE, related_name='subscriptions')
+    stripe_subscription_id = models.CharField(max_length=100, blank=True)
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.student.user.email
+
+
 def post_save_student_create(sender, instance, created, *args, **kwargs):
     if created:
-        Student.objects.create(user=instance)
+        student = Student.objects.create(user=instance)
+        free_pricing = Pricing.objects.get(name='Free')
+        subscription = Subscription.objects.create(student=student, pricing=free_pricing)
+        subscription.save()
+
+def pre_save_pricing(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.name)
+
 
 post_save.connect(post_save_student_create, sender=User)
+pre_save.connect(pre_save_pricing, sender=Pricing)
