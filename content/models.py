f4a8cdcc4d907e8ba3abe1150b5dc99fe91de63c
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-11 09:38:11 +0000

    price row  added to Course object model

--- content/models.py
+++ content/models.py
@@ -27,6 +27,7 @@ class Course(models.Model):
     """ Create Courses which will include list of videos """
     pricing_tiers = models.ManyToManyField(Pricing, blank=True)
     # category = models.ForeignKey(Category, related_name='courses', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, blank=True, unique=True)
     thumbnail = models.ImageField(upload_to='courses/thumbnails/%Y/%m/%d/')
--- content/urls.py
+++ content/urls.py
@@ -5,7 +5,7 @@ from .views import CourseListView, CourseDetailView, VideoDetailView
 # as_view from django documented
 urlpatterns = [
     path('', CourseListView.as_view(), name='course-list'),
-    # path('', CategoryDetailView.as_view(), name='category-list'),
+    # path('category/', CategoryDetailView.as_view(), name='category-list'),
     path('<slug>', CourseDetailView.as_view(), name='course-detail'),
     path('<course_slug>/learn/<slug>', VideoDetailView.as_view(), name='video-detail')
 ]
