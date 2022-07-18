c1066238cc059e48f91ebefc02dcaf0258489cb2
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-18 10:03:10 +0000

    Shopping bag now roking, (Addint products to bag)

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,23 +5,26 @@ from content.models import Course
 
 def bag_contents(request):
 
-    bag_item = []
+    bag_items = []
     total = 0 
     product_count = 0
     bag = request.session.get('bag', {})
 
-    # for course, quantity in bag.items():
-    #     course = get_object_or_404(Course, pk=course.id)
-    #     total += quantity * course.price
-    #     bag_items.append({
-    #         'item_id': course.id,
-    #         'quantity': quantity,
-    #         'course': course,
-    #     })
+    for course_id, quantity in bag.items():
+    # Search the Course table where the primary key is course_id
+        print('bag_item:', bag_items)
+        course = get_object_or_404(Course, pk=course_id)
+        total += quantity * course.price
+        bag_items.append({
+            'item_id': course_id,
+            'quantity': quantity,
+            'course': course,
+        })
+
 
     context = {
 
-        'bag_item': bag_item,
+        'bag_items': bag_items,
         'total': total, 
         'product_count': product_count,
 
--- bag/models.py
+++ bag/models.py
@@ -8,27 +8,27 @@ import stripe
 
 # Create your models here.
 
-class CartAbstract(models.Model):
-     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-     created = models.DateTimeField(auto_now=True)
-     updated = models.DateTimeField(auto_now=True)
+# class CartAbstract(models.Model):
+#      user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+#      created = models.DateTimeField(auto_now=True)
+#      updated = models.DateTimeField(auto_now=True)
 
-     class Meta:
-        abstract = True
+#      class Meta:
+#         abstract = True
 
 
-class CartItem(CartAbstract):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=0)
+# class CartItem(CartAbstract):
+#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+#     quantity = models.IntegerField(default=0)
 
-    def __str__(self):
-        return f"{self.course.name} : {self.quantity}"
+#     def __str__(self):
+#         return f"{self.course.name} : {self.quantity}"
 
     
-class Cart(CartAbstract):
-    items = models.ManyToManyField(CartItem)
-    total_items = models.IntegerField(default=0)
-    total_price = models.FloatField(default=0)
+# class Cart(CartAbstract):
+#     items = models.ManyToManyField(CartItem)
+#     total_items = models.IntegerField(default=0)
+#     total_price = models.FloatField(default=0)
 
-    def __str__(self):
-        return f"{self.user.username},  {self.id}"
+#     def __str__(self):
+#         return f"{self.user.username},  {self.id}"
