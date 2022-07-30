80778e39df3d55f4a6f1f9110c2c878221fc9868
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-30 17:53:22 +0000

    accounts test added and some more javascript to home page for model in the butons and effects in Carousel

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,58 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from .models import Student
 
-# Create your tests here.
+class RegisterPageTests(TestCase):
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/register/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/register.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("register"),
+            {
+                "first_name": "testuser1",
+                "last_name": "testuser2",
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
+
+    
+class LoginPageTests(TestCase):
+
+    def test_url_exists_at_correct_location_loginview(self):
+        response = self.client.get("/accounts/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_name(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/login.html")
+
+    def test_login_form(self):
+        response = self.client.post(
+            reverse("login"),
+            {
+                "username": "testuser",
+                "password": "testpass123",
+                
+            },
+        )
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(get_user_model().objects.all()[0].password, "testpass123")
