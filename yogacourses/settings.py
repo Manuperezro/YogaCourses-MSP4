79cd278ae4c7f2243459bc27c38ba387046ebe8b
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-31 14:41:29 +0000

    Removed secret key and set debug

--- yogacourses/settings.py
+++ yogacourses/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s8icatycn(gr_!d5z$9c3s7-t7jso7l6$3evwu)xp-5ii!-y0c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['rockyoga.herokuapp.com', 'localhost']
 
