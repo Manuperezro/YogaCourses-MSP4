cb23d210f45b9f21be47bfd708a2f898fa2df9f5
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-07-31 15:51:47 +0000

    Deployment AWs_S3

--- yogacourses/settings.py
+++ yogacourses/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
     'checkout',
 
     # Other
-
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -178,6 +178,24 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # a file store the media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+if 'USE_AWS' in os.environ:
+    # S3 Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'rockyoga'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCES_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCES_KEY = os.environ.get('AWS_SECRET_ACCES_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATIFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATIFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
