5ebd7aec24a519c584dcb6e0fb97272e0b71b730
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-08-02 12:38:04 +0000

    AWS round 14

--- yogacourses/settings.py
+++ yogacourses/settings.py
@@ -169,8 +169,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Store ours static files inside a folder called static under the directory membership 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # a file store the media files
 MEDIA_URL = '/media/'
