5fdf76a40426ee08b2110bf412c6454f5d6f78d0
Manuel Pedro Perez Romero <manuperezroomeroo@gmail.com>
2022-08-21 17:41:18 +0000

    Updating Profile

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,8 @@ from django.db.models.signals import post_save
 from django.conf import settings
 import stripe
 
+from django_countries.fields import CountryField
+
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
@@ -13,6 +15,13 @@ class Student(models.Model):
     """ Each user will link by default with one student object """
     # OneToOneField to link each django user with an student user by default
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
     avatar = models.ImageField(default='defatul.png', upload_to='avatar')
     stripe_customer_id = models.CharField(blank=True, null=True, max_length=100)
 
