464bcb25ae33a01daa494f82de1f043af72cb013
Manuel Perez Romero <92267126+Manuperezro@users.noreply.github.com>
2022-12-10 18:52:09 +0000

    migration to GCP

--- yogacourses/settings.py
+++ yogacourses/settings.py
@@ -9,29 +9,39 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import io
 from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
+from google.cloud import secretmanager
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+ALLOWED_HOSTS = ['rockyoga.uk', 'localhost']
+
+if os.environ.get('GOOGLE_CLOUD_PROJECT', None):
+    client = secretmanager.SecretManagerServiceClient()
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    settings_name = os.environ.get("SETTINGS_NAME", "rockyoga")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+    load_dotenv(stream=io.StringIO(payload))
+else:
+    load_dotenv()
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-
-ALLOWED_HOSTS = ['rockyoga.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,7 +127,6 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'yogacourses.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -150,7 +159,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
